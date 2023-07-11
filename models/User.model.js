3d94fc614a784d917d87e1bc8c5630ae1511c789
ariola <ariola.markou@gmail.com>
2023-07-11 22:59:08 +0300

    complete sign up feature

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,16 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
-  },
-  password: String
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: [true, "Username is required"],
+    },
+    passwordHash: {
+        type: String,
+        required: [true, "Password is required"],
+    },
 });
 
 const User = model("User", userSchema);
